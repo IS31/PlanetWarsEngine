6893716f61dc6ad985b341d9ae1d042f7c2d363e
Laurens Rietveld <laurens.rietveld@vu.nl>
2013-01-03 11:25:56 +0100

    made running the engine serial(week1) or parallel (week2) an option

--- Engine.java
+++ Engine.java
@@ -18,6 +18,10 @@ import java.io.*;
 import java.util.*;
 
 public class Engine {
+	
+	public static int GAME_MODE_SERIAL = 1;
+	public static int GAME_MODE_PARALLEL = 2;
+	
     public static void KillClients(List<Process> clients) {
 	for (Process p : clients) {
 	    if (p != null) {
@@ -37,19 +41,22 @@ public class Engine {
 
     public static void main(String[] args) {
 	// Check the command-line arguments.
-	if (args.length != 3 && args.length != 5) {
+	if (args.length < 3 || args.length > 6) {
 	    System.err.println("ERROR: wrong number of command-line " +
 			       "arguments.");
 	    System.err.println("USAGE: java -jar PlayGame.jar <map_file_name> " +
 			       "\"java <player_one>\" " +
-			       "\"java <player_two>\" [<max_num_turns> <max_turn_time>] ");
+			       "\"java <player_two>\" [<game_mode>] [<max_num_turns>] [<max_turn_time>] ");
+	    System.err.println("");
 	    System.err.println("Explanation:");
-	    System.err.println("<map_file_name>: Location of .txt file of map to use for this game");
-	    System.err.println("\"java <player_one>\": Player1. Make sure to add quotes, and add the 'java' part before the bot name. Also make sure your bot is actually compiled (there should be a .class file of your bot file)");
-	    System.err.println("\"java <player_two>\": Idem");
-	    System.err.println("Optional (add both values as argument when deviating from defaults):");
-	    System.err.println("<max_num_turns>: Maximum number of turns this game may take. Default: 100");
-	    System.err.println("<max_turn_time>: Maximum number of time a bot is allowed to take per turn. Default: 100");
+	    System.err.println("<map_file_name>:        Location of .txt file of map to use for this game");
+	    System.err.println("\"java <player_one>\":  Player1. Make sure to add quotes, and add the 'java' part before the bot name. Also make sure your bot is actually compiled (there should be a .class file of your bot file)");
+	    System.err.println("\"java <player_two>\":  Idem");
+	    System.err.println("Optional:");
+	    
+	    System.err.println("<game_mode>:            Game mode to run in. Options are: 'parallel' and 'serial'. Serial (used in week 1) means for each turn, first player1 makes a move, and then player2. Parallel (used in week2) means for every turn, each player makes a move at the same time. Default: serial");
+	    System.err.println("<max_num_turns>:        Maximum number of turns this game may take. Default: 100");
+	    System.err.println("<max_turn_time>:        Maximum number of time a bot is allowed to take per turn. Default: 100");
 	    System.exit(1);
 	}
 	// Initialize the game. Load the map.
@@ -57,11 +64,25 @@ public class Engine {
 	int maxNumTurns = 100;
 	int maxTurnTime = 100; 
 	String logFilename = "log.txt";
+	int gameMode = GAME_MODE_SERIAL;
 	//optional arguments
-	if (args.length == 5) {
-		maxTurnTime = Integer.parseInt(args[4]);
-		maxNumTurns = Integer.parseInt(args[3]);
+	if (args.length == 4) {
+		if (args[3].equalsIgnoreCase(("parallel"))) {
+			gameMode = GAME_MODE_PARALLEL;
+		} else if (args[3].equalsIgnoreCase(("serial"))) {
+			gameMode = GAME_MODE_SERIAL;
+		} else {
+			System.err.println("The 4th argument is unknown. This should either be 'parallel' or 'serial'");
+			System.exit(1);
+		}
 	}
+	if (args.length == 5) {
+		maxNumTurns = Integer.parseInt(args[4]);
+	}
+	if (args.length == 6) {
+		maxTurnTime = Integer.parseInt(args[5]);
+	}
+	
 	Game game = new Game(mapFilename, maxNumTurns, 0, logFilename);
 	if (game.Init() == 0) {
 	    System.err.println("ERROR: failed to start game. map: " +
@@ -99,23 +120,22 @@ public class Engine {
 	    // Send the game state to the clients.
 	    System.err.println("The game state:");
 	    System.err.print(game);
-	    //MODIFIED: send game state only to active player (ap)
-	    //for (int i = 0; i < clients.size(); ++i) {
-		if (clients.get(ap) == null || !game.IsAlive(ap + 1)) {
-		    continue;
-		}
-		String message = game.PovRepresentation(ap + 1) + "go\n";
-		try {
-		    OutputStream out = clients.get(ap).getOutputStream();
-		    OutputStreamWriter writer = new OutputStreamWriter(out);
-		    writer.write(message, 0, message.length());
-		    writer.flush();
-		    //System.err.println("engine > player" + (ap + 1) + ": " +
-			//		 message);
-		} catch (Exception e) {
-		    clients.set(ap, null);
-		}
-	    //}
+	    
+	    if (gameMode == GAME_MODE_SERIAL) {
+	    	//MODIFIED: send game state only to active player (ap)
+	    	if (clients.get(ap) == null || !game.IsAlive(ap + 1)) {
+			    continue;
+			}
+	    	sendGameState(game, clients, ap);
+	    } else {
+	    	for (int i = 0; i < clients.size(); ++i) {
+		    	if (clients.get(i) == null || !game.IsAlive(i + 1)) {
+				    continue;
+				}
+	    	}
+	    	sendGameState(game, clients, ap);
+	    }
+		
 	    // Get orders from the clients.
 	    StringBuilder[] buffers = new StringBuilder[clients.size()];
 	    boolean[] clientDone = new boolean[clients.size()];
@@ -126,26 +146,37 @@ public class Engine {
 	    long startTime = System.currentTimeMillis();
 	    while (!AllTrue(clientDone) &&
 		   System.currentTimeMillis() - startTime < maxTurnTime) {
-		//for (int i = 0 ; i < clients.size(); ++i) {
-	    // MODIFIED: one player per turn
+	    	
+	    	int i;
+	    	int end;
+	    	if (gameMode == GAME_MODE_SERIAL) {
+	    		// MODIFIED: one player per turn
+	    		i = ap;
+	    		end = ap + 1;
+	    	} else {
+	    		i = 0;
+	    		end = clients.size();
+	    	}
+		for (i = 0 ; i < end; ++i) {
 	    
-		    if (!isAlive[ap] || !game.IsAlive(ap + 1) || clientDone[ap]) {
-			clientDone[ap] = true;
+	    	
+		    if (!isAlive[i] || !game.IsAlive(i + 1) || clientDone[i]) {
+			clientDone[i] = true;
 			continue;
 		    }
                     try {
                         InputStream inputStream =
-                            clients.get(ap).getInputStream();
+                            clients.get(i).getInputStream();
                         while (inputStream.available() > 0) {
                             char c = (char)inputStream.read();
                             if (c == '\n') {
-                                String line = buffers[ap].toString().trim();
-                                //System.err.println("P" + (ap+1) + ": " + line);
+                                String line = buffers[i].toString().trim();
+                                //System.err.println("P" + (i+1) + ": " + line);
                                 line = line.toLowerCase().trim();
-                                System.err.println("player" + (ap + 1) + " > engine: " + line);
+                                System.err.println("player" + (i + 1) + " > engine: " + line);
                                 // Modified: only process 1 order
-                                game.IssueOrder(ap + 1, line);
-                                clientDone[ap] = true;
+                                game.IssueOrder(i + 1, line);
+                                clientDone[i] = true;
                                 /*
                                 if (line.equals("go")) {
                                 	clientDone[i] = true;
@@ -153,41 +184,30 @@ public class Engine {
                                 	game.IssueOrder(i + 1, line);
                                 }
                                 */
-                                buffers[ap] = new StringBuilder();
+                                buffers[i] = new StringBuilder();
                                 break;
                             } else {
-                                buffers[ap].append(c);
+                                buffers[i].append(c);
                             }
                         }
-                        StringBuilder buf = new StringBuilder();
-                        InputStream stderr = clients.get(ap).getErrorStream();
-                        while (stderr.available() > 0){
-                            char c = (char)stderr.read();
-                            if (c == '\n') {
-                                String ln = buf.toString();
-                                System.err.println("Player " + (ap+1) + ": " + ln);
-                                buf = new StringBuilder();
-                            }
-                            else {
-                                buf.append(c);
-                            }
-                        }
+                        receiveOrders(clients, i);
                     } catch (Exception e) {
-			System.err.println("WARNING: player " + (ap+1) +
+			System.err.println("WARNING: player " + (i+1) +
 					   " crashed.");
-			clients.get(ap).destroy();
-			game.DropPlayer(ap + 1);
-			isAlive[ap] = false;
+			clients.get(i).destroy();
+			game.DropPlayer(i + 1);
+			isAlive[i] = false;
 		    }
+		}
 		}
 	    //MODIFIED: update active player
 	    ap = (ap + 1)%2;
-	   // }
-	    for (int i = 0 ; i < clients.size(); ++i) {
-		if (!isAlive[i] || !game.IsAlive(i + 1)) {
+	   
+	    for (int j = 0 ; j < clients.size(); ++j) {
+		if (!isAlive[j] || !game.IsAlive(j + 1)) {
 		    continue;
 		}
-		if (clientDone[i]) {
+		if (clientDone[j]) {
 		    continue;
 		}
 		// Do NOT drop players at timeouts
@@ -208,10 +228,6 @@ public class Engine {
 	    //Keep advancing turns, until there are no ships in flight. 
 	    //This way each player has complete knowledge of game state on start
 	    while (game.getFleets().size() != 0) {
-//	    	++numTurns;
-//		    System.err.println("Turn " + numTurns);
-//		    System.out.print(game.FlushGamePlaybackString());
-//		    System.out.flush();
 		    game.skipTimeStep();
 	    }
 	}
@@ -222,7 +238,10 @@ public class Engine {
 	    System.err.println("Draw!");
 	}
 	System.out.println(game.GamePlaybackString());
-    }
+	}
+	
+	
+	
     public void WriteLogMessage(String logFilename, String message) {
     	
         if (logFilename == null) {
@@ -238,4 +257,34 @@ public class Engine {
           // whatev
         }
       }
+    
+    private static void sendGameState(Game game, List<Process> clients, int clientId) {
+    	String message = game.PovRepresentation(clientId + 1) + "go\n";
+		try {
+		    OutputStream out = clients.get(clientId).getOutputStream();
+		    OutputStreamWriter writer = new OutputStreamWriter(out);
+		    writer.write(message, 0, message.length());
+		    writer.flush();
+		    //System.err.println("engine > player" + (ap + 1) + ": " +
+			//		 message);
+		} catch (Exception e) {
+		    clients.set(clientId, null);
+		}
+    }
+    
+    private static void receiveOrders(List<Process> clients, int clientId) throws IOException {
+    	StringBuilder buf = new StringBuilder();
+        InputStream stderr = clients.get(clientId).getErrorStream();
+        while (stderr.available() > 0){
+            char c = (char)stderr.read();
+            if (c == '\n') {
+                String ln = buf.toString();
+                System.err.println("Player " + (clientId+1) + ": " + ln);
+                buf = new StringBuilder();
+            }
+            else {
+                buf.append(c);
+            }
+        }
+    }
 }
